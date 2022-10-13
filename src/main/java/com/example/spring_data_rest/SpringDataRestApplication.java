4004fd5511f913e6a26282db83950f560d3b6489
maksi <maksontuskon@gmail.com>
2022-10-13 15:15:46 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_data_rest/SpringDataRestApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestApplication.class, args);
+    }
+
+}
